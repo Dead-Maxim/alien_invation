ff1a3b1e39016b65f922624e21be6cbbfbf48cd6
Максим Кужненков <maksim-kuzhnenkov@yandex.ru>
2024-05-27 21:57:25 +0300

    настройки

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings():
+    """Класс настроек игры"""
+    # параметры экрана
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_heiht = 800
+        self.bg_color = (230, 230, 230)
