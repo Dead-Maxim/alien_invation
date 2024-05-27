38e93a5cb372a2aa8d58128f6c86a9e3a2142098
Максим Кужненков <maksim-kuzhnenkov@yandex.ru>
2024-05-27 21:20:42 +0300

    комментарии

--- alien_invation.py
+++ alien_invation.py
@@ -1,13 +1,32 @@
 import sys
-import pygame # type: ignore
+import pygame
+
 
 class AlienInvation:
     """Класс для управления ресурсами и поведением игры"""
+
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы"""
+        #инициализация настроек
         pygame.init()
-
+        #запуск окна игры с указанием размера
         self.screen = pygame.display.set_mode((1200, 800))
+        #название окна
         pygame.display.set_caption("Alien Invation")
 
     def run_game(self):
+        """Зауск основного цикла игры"""
+        while True:
+            # Отслеживание событий клавиатуры и мыши
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Отображение последнего прорисованного экрана
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+    # Создание экземпляра и запуск игры
+    ai = AlienInvation()
+    ai.run_game()
