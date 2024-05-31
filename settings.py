194f5944de5e8d5af3903d8f3bdab912eb06d541
Максим Кужненков <maksim-kuzhnenkov@yandex.ru>
2024-05-31 21:25:15 +0300

    изменение скорости корабля

--- settings.py
+++ settings.py
@@ -8,3 +8,5 @@ class Settings():
         self.screen_heiht = 800
         # цвет фона RGB от 0 до 255
         self.bg_color = (230, 230, 230)
+        # скорость перемещения корабля
+        self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
     def __init__(self, ai_game):  # аргумент - объект игры
         """Инициализирует корабль и задает его начальную позицию"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         # получаем данные экрана
         self.screen_rect = ai_game.screen.get_rect()
         # загружает изображение корабля
@@ -16,6 +17,8 @@ class Ship:
         self.rect = self.image.get_rect()
         # корабль появляется у нижненго края
         self.rect.midbottom = self.screen_rect.midbottom
+        # сохранение вещественной координаты корабля
+        self.x = float(self.rect.x)
         # флаги перемещения по умолчанию ложь
         self.moving_right = False
         self.moving_left = False
@@ -24,9 +27,12 @@ class Ship:
         """Обновляет позицию корабля с учетом флагов"""
         # при получении флага True, меняет положение корабля
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        
+        # обновляем rect
+        self.rect.x = self.x
 
     def blitme(self):
         """Рисует корабль в текущей позиции"""
