5914ba7da6d36216f2fd41eb147a760a299cbffb
Максим <maksim-kuzhnenkov@yandex.ru>
2024-06-03 22:04:23 +0300

    параментры снаряда

--- settings.py
+++ settings.py
@@ -10,3 +10,8 @@ class Settings():
         self.bg_color = (230, 230, 230)
         # скорость перемещения корабля
         self.ship_speed = 1.5
+        # параметры снаряда
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullrt_color = (60, 60, 60)
