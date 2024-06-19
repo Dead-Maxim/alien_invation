6df175f6f2a7db47aff0bd3eccee97172d8d8fd8
Максим <maksim-kuzhnenkov@yandex.ru>
2024-06-19 21:48:21 +0300

    превращаем шрека в пришельца

--- shrek.py
+++ shrek.py
@@ -1,21 +1,24 @@
 import pygame
+from pygame.sprite import Sprite
 
 
-class Shrek:
+class Shrek(Sprite):
     """Класc шрека"""
 
     def __init__(self, ai_game):  # аргумент - объект игры
         """Инициализирует шрека и задает его начальную позицию"""
+        super().__init__()
         self.screen = ai_game.screen
-        # получаем данные экрана
-        self.screen_rect = ai_game.screen.get_rect()
-        # загружает изображение корабля
+        # загружает изображение шрека
         image = pygame.image.load("images/shrek.bmp")
         # подгоняем размер шрека
-        self.image = pygame.transform.scale(image, (150, 200))
+        self.image = pygame.transform.scale(image, (75, 100))
         self.rect = self.image.get_rect()
         # шрек появляется в центре
-        self.rect.center = self.screen_rect.center
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        # сохранение горизонтальной позиции
+        self.x = float(self.rect.x)
 
     def blitme(self):
         """Рисует шрека в текущей позиции"""
